b1f175fc018942f2394de17a4fcf236ff0db977a
Roula Sharqawe <Roula@MacBook-Pro-3.frontierlocal.net>
2018-03-22 00:39:04 -0400

    added delete alert+edit tours

--- workspace/components/ManageTours/ManageToursContainer.js
+++ workspace/components/ManageTours/ManageToursContainer.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableOpacity, TouchableHighlight, Button, ImageBackground, Image, TextInput,ScrollView} from 'react-native';
+import { Alert, View, Text, TouchableOpacity, TouchableHighlight, Button, ImageBackground, Image, TextInput,ScrollView} from 'react-native';
 import { connect } from 'react-redux';
 
 import Icon from 'react-native-vector-icons/Entypo';
@@ -10,6 +10,7 @@ import { TimeDate, OneDateTime } from '../TimeDate';
 import HideableView from 'react-native-hideable-view';
 import { Calendar } from 'react-native-calendars';
 import DateTimePicker from 'react-native-modal-datetime-picker';
+import {editTours} from '../../actions/CreateTours';
 
 import { connectAlert } from '../../components/Alert';
 
@@ -19,6 +20,61 @@ const profilePic = require('../Container/profilePic.png');
 const tour2 = require('../../assets/images/tour2.jpeg');
 
 class ManageToursContainer extends Component {
+
+
+    deleteAlert = () => {
+
+      return (
+            Alert.alert(
+            'Are you sure you want to delete this tour?',
+            'You cannot undo this action.',
+            [
+                {text: 'Delete', onPress: () => console.log('delete pressed') },//this.props.dispatch(deleteTours(this.props.token,this.props.email,data.id) },
+                {text: 'Cancel', onPress: () => console.log('Cancel pressed'), style: 'cancel'},
+                
+            ],
+            { cancelable: false }
+            )
+      )
+    }
+
+    renderOldInfo = (data) => {
+      
+          console.log("here");
+
+          const tourInfo = {
+                 
+                      city: data.city,
+                      title: data.title,
+                      duration: data.duration,
+                      description: data.description,
+                      tourID: data.id,
+                      max_persons: data.max_persons,
+                      is_daytrip: data.is_daytrip,
+                      is_foodie: data.is_foodie,
+                      is_landmarks: data.is_landmarks,
+                      is_museums: data.is_museums,
+                      is_nightlife: data.is_nightlife,
+                      is_outdoors: data.is_outdoors, 
+                      price: data.price,
+                  }   
+              
+              this.props.dispatch(editTours(tourInfo))
+        
+    }
+
+              componentWillReceiveProps(nextProps) {
+              //if errors alert
+              if(nextProps.tourInfo && nextProps.tourInfo !== this.props.tourInfo) {
+                    this.props.navigation.navigate('CreateTours');
+              //if success
+              } 
+            
+               
+            
+            }         
+
+    
 
     render() {
     	return (
@@ -31,6 +87,7 @@ class ManageToursContainer extends Component {
         name="plus"
         style={styles.plusIcon} size={65} 
         onPress={()=> {this.props.navigation.navigate('CreateTours');}} />
+
    	</TouchableOpacity>
     </View>
         {this.props.tours.map((data) => {
@@ -47,10 +104,12 @@ class ManageToursContainer extends Component {
                     <Icon3
         			name="edit"
        				style={styles.editIcon} size={21} 
-              onPress={()=> {this.props.navigation.navigate('CreateTours');}}/>
+              onPress={()=> {this.renderOldInfo(data)}} />
+
+              
 
        				</TouchableOpacity>
-       				<TouchableOpacity style= {styles.deletePress}>
+       				<TouchableOpacity style= {styles.deletePress} onPress = {() => this.deleteAlert()} >
    					<Icon2
         			name="delete"
        				style={styles.deleteIcon} size={25} />
@@ -87,9 +146,16 @@ class ManageToursContainer extends Component {
 const mapStateToProps = (state) => {
 
     const tours = state.ViewTours.result.tours;
+    const authentication_token = state.ViewTours.authentication_token;
+    const email = state.ViewTours.email;
+    const tourInfo = state.CreateTours.tourInfo;
+
 
     return {
-        tours
+        tours,
+        email,
+        authentication_token,
+        tourInfo,
     }
 
 }
